7d36d1e2792beb6fb80fdbb54824007df934108a
1lhc <90663580+1lhc@users.noreply.github.com>
2023-06-01 20:15:56 -0700

    1

--- Source/Student/Project_2/P2_Pathfinding.cpp
+++ Source/Student/Project_2/P2_Pathfinding.cpp
@@ -5,90 +5,140 @@
 #pragma region Extra Credit
 bool ProjectTwo::implemented_floyd_warshall()
 {
-    return false;
+	return false;
 }
 
 bool ProjectTwo::implemented_goal_bounding()
 {
-    return false;
+	return false;
 }
 
 bool ProjectTwo::implemented_jps_plus()
 {
-    return false;
+	return false;
 }
 #pragma endregion
 
 bool AStarPather::initialize()
 {
-    // handle any one-time setup requirements you have
+	// handle any one-time setup requirements you have
 
-    /*
-        If you want to do any map-preprocessing, you'll need to listen
-        for the map change message.  It'll look something like this:
+	/*
+		If you want to do any map-preprocessing, you'll need to listen
+		for the map change message.  It'll look something like this:
 
-        Callback cb = std::bind(&AStarPather::your_function_name, this);
-        Messenger::listen_for_message(Messages::MAP_CHANGE, cb);
+		Callback cb = std::bind(&AStarPather::your_function_name, this);
+		Messenger::listen_for_message(Messages::MAP_CHANGE, cb);
 
-        There are other alternatives to using std::bind, so feel free to mix it up.
-        Callback is just a typedef for std::function<void(void)>, so any std::invoke'able
-        object that std::function can wrap will suffice.
-    */
+		There are other alternatives to using std::bind, so feel free to mix it up.
+		Callback is just a typedef for std::function<void(void)>, so any std::invoke'able
+		object that std::function can wrap will suffice.
+	*/
 
-    return true; // return false if any errors actually occur, to stop engine initialization
+	return true; // return false if any errors actually occur, to stop engine initialization
 }
 
 void AStarPather::shutdown()
 {
-    /*
-        Free any dynamically allocated memory or any other general house-
-        keeping you need to do during shutdown.
-    */
+	/*
+		Free any dynamically allocated memory or any other general house-
+		keeping you need to do during shutdown.
+	*/
 }
 
-PathResult AStarPather::compute_path(PathRequest &request)
+PathResult AStarPather::compute_path(PathRequest& request)
 {
-    /*
-        This is where you handle pathing requests, each request has several fields:
+	/*
+		This is where you handle pathing requests, each request has several fields:
 
-        start/goal - start and goal world positions
-        path - where you will build the path upon completion, path should be
-            start to goal, not goal to start
-        heuristic - which heuristic calculation to use
-        weight - the heuristic weight to be applied
-        newRequest - whether this is the first request for this path, should generally
-            be true, unless single step is on
+		start/goal - start and goal world positions
+		path - where you will build the path upon completion, path should be
+			start to goal, not goal to start
+		heuristic - which heuristic calculation to use
+		weight - the heuristic weight to be applied
+		newRequest - whether this is the first request for this path, should generally
+			be true, unless single step is on
 
-        smoothing - whether to apply smoothing to the path
-        rubberBanding - whether to apply rubber banding
-        singleStep - whether to perform only a single A* step
-        debugColoring - whether to color the grid based on the A* state:
-            closed list nodes - yellow
-            open list nodes - blue
+		smoothing - whether to apply smoothing to the path
+		rubberBanding - whether to apply rubber banding
+		singleStep - whether to perform only a single A* step
+		debugColoring - whether to color the grid based on the A* state:
+			closed list nodes - yellow
+			open list nodes - blue
 
-            use terrain->set_color(row, col, Colors::YourColor);
-            also it can be helpful to temporarily use other colors for specific states
-            when you are testing your algorithms
+			use terrain->set_color(row, col, Colors::YourColor);
+			also it can be helpful to temporarily use other colors for specific states
+			when you are testing your algorithms
 
-        method - which algorithm to use: A*, Floyd-Warshall, JPS+, or goal bounding,
-            will be A* generally, unless you implement extra credit features
+		method - which algorithm to use: A*, Floyd-Warshall, JPS+, or goal bounding,
+			will be A* generally, unless you implement extra credit features
 
-        The return values are:
-            PROCESSING - a path hasn't been found yet, should only be returned in
-                single step mode until a path is found
-            COMPLETE - a path to the goal was found and has been built in request.path
-            IMPOSSIBLE - a path from start to goal does not exist, do not add start position to path
-    */
+		The return values are:
+			PROCESSING - a path hasn't been found yet, should only be returned in
+				single step mode until a path is found
+			COMPLETE - a path to the goal was found and has been built in request.path
+			IMPOSSIBLE - a path from start to goal does not exist, do not add start position to path
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
+	//If(request.newRequest) {
+	//	Initialize everything.Clear Open / Closed Lists.
+	//		Push Start Node onto the Open List with cost of f(x) = g(x) + h(x).
+	//}
+	//While(Open List is not empty) {
+	//	parentNode = Pop cheapest node off Open List.
+	//		If parentNode is the Goal Node, then path found(return PathResult::COMPLETE).
+	//		Place parentNode on the Closed List.
+	//		For(all valid neighboring child nodes of parentNode) {
+	//		Compute its cost, f(x) = g(x) + h(x)
+	//			If child node isn’t on Open or Closed list, put it on Open List.
+	//			Else if child node is on Open or Closed List, AND this new one is cheaper,
+	//			then take the old expensive one off both listsand put this new
+	//			cheaper one on the Open List.
+	//	}
+	//	If taken too much time this frame(or if request.settings.singleStep == true),
+	//		abort search for nowand resume next frame(return PathResult::PROCESSING).
+	//}
+	//Open List empty, thus no path possible(return PathResult::IMPOSSIBLE).
 
-    
-    // Just sample code, safe to delete
-    GridPos start = terrain->get_grid_position(request.start);
-    GridPos goal = terrain->get_grid_position(request.goal);
-    terrain->set_color(start, Colors::Orange);
-    terrain->set_color(goal, Colors::Orange);
-    request.path.push_back(request.start);
-    request.path.push_back(request.goal);
-    return PathResult::COMPLETE;
+
+	// Just sample code, safe to delete
+	GridPos start = terrain->get_grid_position(request.start);
+	GridPos goal = terrain->get_grid_position(request.goal);
+	terrain->set_color(start, Colors::Orange);
+	terrain->set_color(goal, Colors::Orange);
+	request.path.push_back(request.start);
+	request.path.push_back(request.goal);
+	return PathResult::COMPLETE;
+}
+
+void AStarPather::clear_all_nodes()
+{
+	MaxMap = OriginalMap;
+
+}
+
+void AStarPather::push_node(Node* add)
+{
+	OpenList.push_back(add);
+}
+
+void AStarPather::pop_node()
+{
+	int cheapestIndex = 0;  // Assume the first node is the cheapest
+	float cheapestValue = OpenList[0]->finalCost;
+
+	for (int i = 1; i < OpenList.size(); ++i) {
+		if (OpenList[i]->finalCost < cheapestValue) {
+			cheapestIndex = i;
+			cheapestValue = OpenList[i]->finalCost;
+		}
+	}
+
+	OpenList[cheapestIndex] = OpenList.back();
+	OpenList.pop_back();
+}
+
+void AStarPather::update_node()
+{
 }
--- Source/Student/Project_2/P2_Pathfinding.h
+++ Source/Student/Project_2/P2_Pathfinding.h
@@ -4,23 +4,41 @@
 class AStarPather
 {
 public:
-    /* 
-        The class should be default constructible, so you may need to define a constructor.
-        If needed, you can modify the framework where the class is constructed in the
-        initialize functions of ProjectTwo and ProjectThree.
-    */
+	/*
+		The class should be default constructible, so you may need to define a constructor.
+		If needed, you can modify the framework where the class is constructed in the
+		initialize functions of ProjectTwo and ProjectThree.
+	*/
 
-    /* ************************************************** */
-    // DO NOT MODIFY THESE SIGNATURES
-    bool initialize();
-    void shutdown();
-    PathResult compute_path(PathRequest &request);
-    /* ************************************************** */
+	/* ************************************************** */
+	// DO NOT MODIFY THESE SIGNATURES
+	bool initialize();
+	void shutdown();
+	PathResult compute_path(PathRequest& request);
+	/* ************************************************** */
 
-    /*
-        You should create whatever functions, variables, or classes you need.
-        It doesn't all need to be in this header and cpp, structure it whatever way
-        makes sense to you.
-    */
+	/*
+		You should create whatever functions, variables, or classes you need.
+		It doesn't all need to be in this header and cpp, structure it whatever way
+		makes sense to you.
+	*/
+	enum class onList { Open, Closed, Not };
+	struct Node {
+		Node* parent;			// Parent
+		GridPos gridPos;		// Node’s location
+		float finalCost;		// Final cost f(x)
+		float givenCost;		// Given cost g(x)
+		onList nodeState;		// On open/closed list?
+	};
 
+	std::vector<Node> MaxMap;
+	std::vector<Node> OriginalMap;
+
+	void clear_all_nodes();
+
+	std::vector<Node*> OpenList;
+
+	void push_node(Node* add);
+	void pop_node();
+	void update_node();
 };
